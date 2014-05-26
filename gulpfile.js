fcb8327d002d4f791d7df65e0496a9f173c88322
Shane Wilson <shanezilla@gmail.com>
2014-05-26 16:57:49 -0400

    fixes some jslinting issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp');
-var pg = require("gulp-load-plugins")({
+var $ = require("gulp-load-plugins")({
   scope: ["devDependencies"], // which keys in the config to look within
   camelize: true, // if true, transforms hyphenated plugins names to camel case
   lazy: true
@@ -11,64 +11,35 @@ var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var reactify = require('reactify');
-//var react = require('gulp-react');
-//var jshint = require('gulp-jshint-cached');
 var browserSync = require('browser-sync');
-var gutil = require('gulp-util');
-//var gulpif = require('gulp-if');
-//var uglify = require('gulp-uglify');
-//var clean = require('gulp-clean');
-//var prettify = require('gulp-html-prettify');
-//var size = require('gulp-filesize');
-//var rename = require("gulp-rename");
-//var jscs = require('gulp-jscs');
-//var notify = require('gulp-notify');
-//var plumber = require('gulp-plumber');
 var Notification = require('node-notifier');
-//var minifyHTML = require('gulp-minify-html');
-//var sourcemaps = require('gulp-sourcemaps');
-//var imagemin = require('gulp-imagemin');
 var pngcrush = require('imagemin-pngcrush');
-//var stripDebug = require('gulp-strip-debug');
-//var changed = require('gulp-changed');
 
 var config = require('./gulp-config.json');
-var packageJson = require('./package.json');
-var dependencies = Object.keys(packageJson && packageJson.dependencies || {});
+var dependencies = Object.keys(require('./package.json').dependencies);
 
 var notifier = new Notification();
-var production = !!gutil.env.production;
+var production = !!$.util.env.production;
 var watch = false;
-console.log(pg);
-gutil.log('Environment', gutil.colors.blue(production ? 'Production' : 'Development'));
+
+$.util.log('Environment', $.util.colors.blue(production ? 'Production' : 'Development'));
 
 gulp.task('clean', function() {
   return gulp.src(config.paths.dest.path, {read: false})
-    .pipe(pg.clean());
+    .pipe($.clean());
 });
 
 gulp.task('jslint', function() {
   return gulp.src(config.paths.src.js.glob)
-    .pipe(pg.react())
-    .pipe(pg.jscs())
+    .pipe($.react())
+    .pipe($.jscs())
     .on('error', function(e) {
-      notifier.notify({
-        title: 'JSCS Error',
-        message: 'JSCS Error'
-      });
-      gutil.log(e.message);
-      process.exit(0);
+      $.util.log(e.message);
+      if (!watch) process.exit(0);
     })
-    .pipe(pg.jshint('.jshintrc'))
-    .pipe(pg.jshint.reporter('jshint-stylish'))
-    .pipe(pg.jshint.reporter('fail'))
-    .on('error', function(e) {
-      notifier.notify({
-        title: 'JSHint Error',
-        message: e
-      });
-      process.exit(0);
-    })
+    .pipe($.jshint('.jshintrc'))
+    .pipe($.jshint.reporter('jshint-stylish'))
+    .pipe($.if(!watch, $.jshint.reporter('fail')))
 });
 
 gulp.task('js-libs', function() {
@@ -77,19 +48,19 @@ gulp.task('js-libs', function() {
     .bundle()
     .pipe(source('libs.js'))
     .pipe(buffer())
-    .pipe(pg.filesize())
+    .pipe($.filesize())
     .pipe(gulp.dest(config.paths.dest.js));
 
   if (production) {
     stream.pipe(buffer())
-      .pipe(pg.uglify(
+      .pipe($.uglify(
         {
           mangle: {
             except: ['require', 'export', '$.super']
           }
         }))
-      .pipe(pg.rename('libs.min.js'))
-      .pipe(pg.filesize())
+      .pipe($.rename('libs.min.js'))
+      .pipe($.filesize())
       .pipe(gulp.dest(config.paths.dest.js))
   }
   return stream;
@@ -106,22 +77,22 @@ gulp.task('js', ['js-libs'], function() {
       })
       .pipe(source('bundle.js'))
       .pipe(buffer())
-      .pipe(pg.filesize())
+      .pipe($.filesize())
       .pipe(gulp.dest(config.paths.dest.js));
 
     if (production) {
       stream.pipe(buffer())
-        .pipe(pg.stripDebug())
-        .pipe(pg.sourcemaps.init())
-        .pipe(pg.uglify(
+        .pipe($.stripDebug())
+        .pipe($.sourcemaps.init())
+        .pipe($.uglify(
           {
             mangle: {
               except: ['require', 'export', 'pgsuper']
             }
           }))
-        .pipe(pg.rename('bundle.min.js'))
-        .pipe(pg.sourcemaps.write('./'))
-        .pipe(pg.filesize())
+        .pipe($.rename('bundle.min.js'))
+        .pipe($.sourcemaps.write('./'))
+        .pipe($.filesize())
         .pipe(gulp.dest(config.paths.dest.js))
     }
 
@@ -140,8 +111,8 @@ gulp.task('js', ['js-libs'], function() {
 
 gulp.task('images', function () {
   return gulp.src(config.paths.src.images)
-    .pipe(pg.changed(config.paths.dest.images))
-    .pipe(pg.imagemin({
+    .pipe($.newer(config.paths.dest.images))
+    .pipe($.imagemin({
       optimizationLevel: 3,
       progressive: true,
       interlaced: true,
@@ -153,9 +124,9 @@ gulp.task('images', function () {
 
 gulp.task('html', function() {
   return gulp.src(config.paths.src.html)
-    .pipe(pg.if(production,
-      pg.minifyHtml({conditionals: true, cdata: true, empty: true}),
-      pg.htmlPrettify({indent_char: ' ', indent_size: 2})))
+    .pipe($.if(production,
+      $.minifyHtml({conditionals: true, cdata: true, empty: true}),
+      $.htmlPrettify({indent_char: ' ', indent_size: 2})))
     .pipe(gulp.dest(config.paths.dest.path));
 });
 
--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -3,7 +3,7 @@
 var React = require('react');
 
 var Hello = require('./components/hello.jsx');
-console.log('here');
+
 React.renderComponent(
   new Hello({
     page: ''//router.getRoute(0)
