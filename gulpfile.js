66c03becc6322d4b4616cbd1f1617356f0e03d52
Shane Wilson <shanezilla@gmail.com>
2014-05-26 16:19:26 -0400

    imagemin, autoload plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
 'use strict';
 
 var gulp = require('gulp');
+var pg = require("gulp-load-plugins")({
+  scope: ["devDependencies"], // which keys in the config to look within
+  camelize: true, // if true, transforms hyphenated plugins names to camel case
+  lazy: true
+});
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var watchify = require('watchify');
 var browserify = require('browserify');
 var reactify = require('reactify');
-var react = require('gulp-react');
-var jshint = require('gulp-jshint-cached');
+//var react = require('gulp-react');
+//var jshint = require('gulp-jshint-cached');
 var browserSync = require('browser-sync');
 var gutil = require('gulp-util');
-var gulpif = require('gulp-if');
-var uglify = require('gulp-uglify');
-var clean = require('gulp-clean');
-var prettify = require('gulp-html-prettify');
-var size = require('gulp-filesize');
-var rename = require("gulp-rename");
-var jscs = require('gulp-jscs');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
+//var gulpif = require('gulp-if');
+//var uglify = require('gulp-uglify');
+//var clean = require('gulp-clean');
+//var prettify = require('gulp-html-prettify');
+//var size = require('gulp-filesize');
+//var rename = require("gulp-rename");
+//var jscs = require('gulp-jscs');
+//var notify = require('gulp-notify');
+//var plumber = require('gulp-plumber');
 var Notification = require('node-notifier');
-var minifyHTML = require('gulp-minify-html');
-var sourcemaps = require('gulp-sourcemaps');
+//var minifyHTML = require('gulp-minify-html');
+//var sourcemaps = require('gulp-sourcemaps');
+//var imagemin = require('gulp-imagemin');
+var pngcrush = require('imagemin-pngcrush');
+//var stripDebug = require('gulp-strip-debug');
+//var changed = require('gulp-changed');
 
 var config = require('./gulp-config.json');
 var packageJson = require('./package.json');
@@ -30,18 +39,18 @@ var dependencies = Object.keys(packageJson && packageJson.dependencies || {});
 var notifier = new Notification();
 var production = !!gutil.env.production;
 var watch = false;
-
+console.log(pg);
 gutil.log('Environment', gutil.colors.blue(production ? 'Production' : 'Development'));
 
 gulp.task('clean', function() {
   return gulp.src(config.paths.dest.path, {read: false})
-    .pipe(clean());
+    .pipe(pg.clean());
 });
 
 gulp.task('jslint', function() {
   return gulp.src(config.paths.src.js.glob)
-    .pipe(react())
-    .pipe(jscs())
+    .pipe(pg.react())
+    .pipe(pg.jscs())
     .on('error', function(e) {
       notifier.notify({
         title: 'JSCS Error',
@@ -50,9 +59,9 @@ gulp.task('jslint', function() {
       gutil.log(e.message);
       process.exit(0);
     })
-    .pipe(jshint.cached('.jshintrc'))
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
+    .pipe(pg.jshint('.jshintrc'))
+    .pipe(pg.jshint.reporter('jshint-stylish'))
+    .pipe(pg.jshint.reporter('fail'))
     .on('error', function(e) {
       notifier.notify({
         title: 'JSHint Error',
@@ -68,19 +77,19 @@ gulp.task('js-libs', function() {
     .bundle()
     .pipe(source('libs.js'))
     .pipe(buffer())
-    .pipe(size())
+    .pipe(pg.filesize())
     .pipe(gulp.dest(config.paths.dest.js));
 
   if (production) {
     stream.pipe(buffer())
-      .pipe(uglify(
+      .pipe(pg.uglify(
         {
           mangle: {
-            except: ['require', 'export', '$super']
+            except: ['require', 'export', '$.super']
           }
         }))
-      .pipe(rename('libs.min.js'))
-      .pipe(size())
+      .pipe(pg.rename('libs.min.js'))
+      .pipe(pg.filesize())
       .pipe(gulp.dest(config.paths.dest.js))
   }
   return stream;
@@ -97,21 +106,22 @@ gulp.task('js', ['js-libs'], function() {
       })
       .pipe(source('bundle.js'))
       .pipe(buffer())
-      .pipe(size())
+      .pipe(pg.filesize())
       .pipe(gulp.dest(config.paths.dest.js));
 
     if (production) {
       stream.pipe(buffer())
-        .pipe(sourcemaps.init())
-        .pipe(uglify(
+        .pipe(pg.stripDebug())
+        .pipe(pg.sourcemaps.init())
+        .pipe(pg.uglify(
           {
             mangle: {
-              except: ['require', 'export', '$super']
+              except: ['require', 'export', 'pgsuper']
             }
           }))
-        .pipe(rename('bundle.min.js'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(size())
+        .pipe(pg.rename('bundle.min.js'))
+        .pipe(pg.sourcemaps.write('./'))
+        .pipe(pg.filesize())
         .pipe(gulp.dest(config.paths.dest.js))
     }
 
@@ -128,11 +138,24 @@ gulp.task('js', ['js-libs'], function() {
   return rebundle();
 });
 
+gulp.task('images', function () {
+  return gulp.src(config.paths.src.images)
+    .pipe(pg.changed(config.paths.dest.images))
+    .pipe(pg.imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest(config.paths.dest.images));
+});
+
 gulp.task('html', function() {
   return gulp.src(config.paths.src.html)
-    .pipe(gulpif(production,
-      minifyHTML({conditionals: true, cdata: true, empty: true}),
-      prettify({indent_char: ' ', indent_size: 2})))
+    .pipe(pg.if(production,
+      pg.minifyHtml({conditionals: true, cdata: true, empty: true}),
+      pg.htmlPrettify({indent_char: ' ', indent_size: 2})))
     .pipe(gulp.dest(config.paths.dest.path));
 });
 
@@ -160,5 +183,5 @@ gulp.task('default', function() {
 });
 
 gulp.task('build', ['clean'], function() {
-  return gulp.start('html', 'js');
+  return gulp.start('html', 'images','js');
 });
