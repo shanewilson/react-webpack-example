8132c077870d41fbb08c9b2562b6204ea5cc4a75
Shane Wilson <shane.wilson@oicr.on.ca>
2015-02-26 21:53:46 -0500

    feat(a11y): adds react-a11y
    - found error when it was forced so it works,
    but apparently there are no a11y errors?
    
    Closes #7

--- src/entry.jsx
+++ src/entry.jsx
@@ -4,6 +4,9 @@ import React from "react/addons"
 import Router from 'react-router'
 import routes from './js/routes.jsx'
 
+import a11y from 'react-a11y'
+a11y({throw: true});
+
 document.addEventListener("DOMContentLoaded", (event) => {
   Router.run(routes, Router.HistoryLocation, (Handler, state) => {
     React.render(<Handler/>, document.getElementById("app"))
